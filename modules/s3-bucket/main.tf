99e1e3dae7e096d9b5038ebf51eebf21767e98c6
Byungjin Park <posquit0.bj@gmail.com>
2026-06-14 12:13:47 +0900

    fix(s3-bucket): remove undefined var.object_lock.token reference

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -32,7 +32,7 @@ resource "aws_s3_bucket" "this" {
   bucket        = var.name
   force_destroy = var.force_destroy
 
-  object_lock_enabled = var.object_lock.enabled && var.object_lock.token == ""
+  object_lock_enabled = var.object_lock.enabled
 
   tags = merge(
     {
